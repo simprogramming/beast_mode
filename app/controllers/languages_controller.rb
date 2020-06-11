f611a3ac346ad65ac58738291d9f78e259df9245
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-11 18:34:32 -0400

    Added favicon and coded languages controller

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,13 +1,35 @@
 class LanguagesController < ApplicationController
+  before_action :find_language, only: [:edit, :update]
+
   def index
+    @languages = Language.all
   end
 
   def new
+    @language = Language.new
+  end
+
+  def create
+    @language= Language.new(language_params)
   end
 
   def edit
   end
-
+  
   def update
+    if @language.update(language_params)
+      flash.notice = "Language successfully created!"
+    else
+      render :new
+    end
+  end
+  
+  private
+  def find_language
+    @language = Language.find(params[:id])
+  end
+
+  def language_params
+    params.require(:language).permit(:name)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Language.destroy_all
+User.destroy_all
+
+puts "Creating users"
+steph = User.create!(username: "steph", password: "123456")
+sim = User.create!(username: "sim", password: "123456")
+
+puts "Creating languages"
+ruby = Language.create!(name: "Ruby")
+html = Language.create!(name: "HTML")
+css = Language.create!(name: "CSS")
+javascript = Language.create!(name: "JavaScript")
+react = Language.create!(name: "React")
+node = Language.create!(name: "Node")
+python = Language.create!(name: "Python")
+
+puts "Creating courses"
+
