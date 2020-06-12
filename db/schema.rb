b1c4548b3e9fdba43444d1884a276f7b692b6745
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-11 21:05:50 -0400

    solved sign in issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_11_232234) do
-
 ActiveRecord::Schema.define(version: 2020_06_11_230107) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
