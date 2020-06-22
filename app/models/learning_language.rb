90ad9cfee5a7cfdf5e72f948e93f2ef4fe1406fa
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-22 15:22:55 -0400

    Added learning language controller

--- app/models/learning_language.rb
+++ app/models/learning_language.rb
@@ -2,5 +2,7 @@ class LearningLanguage < ApplicationRecord
   belongs_to :language
   belongs_to :user
 
+  validates :language, uniqueness: true, presence: true
+  validates :user, presence: true
   # validates :language_id, presence :true, inclusion: { in: [ Language.all ], message: "%{value} is not a language in the database. Please add it to the database first." }
 end
