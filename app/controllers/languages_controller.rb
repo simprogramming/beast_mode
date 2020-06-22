2d3dd905dcd5ea7782a7999cefa8f404581df19a
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-22 12:15:58 -0400

    re-styled navbar

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,7 +2,8 @@ class LanguagesController < ApplicationController
   before_action :find_language, only: [:show, :edit, :update]
 
   def index
-    @learning_languages = current_user.learning_languages
+    @languages = Language.all
+    # @learning_languages = current_user.learning_languages
   end
 
   def show
