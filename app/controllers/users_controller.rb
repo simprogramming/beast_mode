ebe0497e52204deb47b7e5fa802369dde47d3171
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-23 19:12:52 -0400

    Added backend logic for currently learning and currently taking cards on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    
     @languages = LearningLanguage.where(user: @user)
+    @currently_learning_languages = @languages.select { |learning_language| learning_language.currently_learning == true }
+
     @courses = TakingCourse.where(user: @user)
+    @currently_taking_courses = @courses.select { |taking_course| taking_course.currently_taking == true }
   end
 
   def edit
