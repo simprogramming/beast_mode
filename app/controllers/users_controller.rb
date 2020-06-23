db35fd316effa8801d6f0c5aa1414d41b276330e
Stephanie Diep <stephanie.diep002@gmail.com>
2020-06-23 16:05:47 -0400

    Added basic backend code to my profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @languages = LearningLanguage.where(user: @user)
+    @courses = TakingCourse.where(user: @user)
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
